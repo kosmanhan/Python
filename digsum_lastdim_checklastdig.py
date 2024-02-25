e644d5ef4df5f331d2bc27e4af0b79218dd81501
Osman Khan <kosmanhan@outlook.com>
2024-02-24 19:16:23 -0500

    Update digsum_lastdim_checklastdig.py

--- digsum_lastdim_checklastdig.py
+++ digsum_lastdim_checklastdig.py
@@ -1,21 +1,21 @@
 #finding the digital sum of a number, n
 def digsum(n):
     digsum=0
-  while len(str(n))>1:
-    for i in range(len(str(n))):
-      digsum=digsum+int(str(n)[i])
-    n=digsum
-  return n
+    while len(str(n))>1:
+        for i in range(len(str(n))):
+            digsum=digsum+int(str(n)[i])
+        n=digsum
+    return n
 
 #finding the last digit of a number, n
 def lastdig(n):
-  return int((str(n))[-1])
+    return int((str(n))[-1])
 
 #checking the last digit to determine evenness
 def evennum(n):
-  t=lastdig(n)
-  even=[0,2,4,6,8]
-  if t in even:
-    return True
-  else:
-    return False
+    t=lastdig(n)
+    even=[0,2,4,6,8]
+    if t in even:
+        return True
+    else:
+        return False
